de225f10ff14f3415df8a4527eee8442422665b4
finite07 <67774134+finite07@users.noreply.github.com>
2023-02-04 11:47:13 +0530

    Update Solution2.java

--- Reverse_Number/Solution2.java
+++ Reverse_Number/Solution2.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
-public class Solution{
-	public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        StringBuffer str=new StringBuffer();
-        str=str.append(number);
-        System.out.print("The Reverse of the number is "+ str.reverse());
-	}
+
+class test{
+        public static void main(String[] args){
+            Scanner scan=new Scanner(System.in);
+                String str= scan.next();
+               String nstr=" ";
+               char ch;
+
+               for(int i=0;i<str.length();i++){
+                   ch=str.charAt(i);
+                   nstr=ch+nstr;
+
+
+
+               }
+            System.out.println(nstr);
+
+
+
+        }
 }
